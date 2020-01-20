a35a68783706943105ac666489399109e068bb82
Adel Araji <adel_araji@hotmail.com>
2020-01-20 15:15:35 -0500

    Added Activities for the different screens

---
+++ app/src/main/java/com/example/seg_lab1/QuestionScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.seg_lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuestionScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_screen);
+    }
+}
---
+++ app/src/main/java/com/example/seg_lab1/SummaryScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.seg_lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SummaryScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary_screen);
+    }
+}
