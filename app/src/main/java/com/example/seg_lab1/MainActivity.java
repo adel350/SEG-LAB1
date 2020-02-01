b8d8cf62986d6b564b1e85a794969764ad193913
Udoka Nwabuike <nudok079@uottawa.ca>
2020-02-01 16:27:25 -0500

    Update to Test2 completed

--- app/src/main/java/com/example/seg_lab1/MainActivity.java
+++ app/src/main/java/com/example/seg_lab1/MainActivity.java
@@ -31,17 +31,17 @@ public class MainActivity extends AppCompatActivity {
 //        System.out.println(loadJSONFromAsset());
 
         questions = new ArrayList<Question>();
-        populateData();
-
-        //Populate all questions and settings scheme HERE
+         //Populate all questions and settings scheme HERE
     }
 
     public void testOnClick(View view) {
+        populateData();
         Intent intent = new Intent(this, QuestionScreenActivity.class);
         intent.putExtra("questionsList", questions); //passing the questions array to the next activity
         startActivity(intent);
     }
     public void test2OnClick(View view){
+        populateData2();
         Intent intent = new Intent(this, QuestionScreenActivity.class);
         intent.putExtra("questionsList", questions); //passing the questions array to the next activity
         startActivity(intent);
@@ -68,16 +68,28 @@ public class MainActivity extends AppCompatActivity {
         }
         return json;
     }
+    //Method to return the 2nd JSON stored in assets as a String
+    public String loadJSON2FromAsset() {
+        String json = null;
+        try {
+            InputStream is = this.getAssets().open("db2.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
 
     public void populateData() {
         try {
             JSONObject obj = new JSONObject(loadJSONFromAsset());
             JSONArray questionArray = obj.getJSONArray("questions");
             JSONArray optionsArray = obj.getJSONArray("choices");
-//            ArrayList<HashMap<String, String>> questionList = new ArrayList<HashMap<String, String>>();
-//            HashMap<String, String> m_li;
-
-
 
             for (int i = 0; i < questionArray.length(); i++) {
                 JSONObject jo_inside = questionArray.getJSONObject(i);
@@ -88,36 +100,52 @@ public class MainActivity extends AppCompatActivity {
 
                 Question newQuestion = new Question(question_id, title, correctAnswerId);
                 questions.add(newQuestion);
-
-
-                //For each question, add the corresponding options
-
-
-//                //Add your values in your `ArrayList` as below:
-//                m_li = new HashMap<String, String>();
-//                m_li.put("formule", formula_value);
-//                m_li.put("url", url_value);
-//
-//                formList.add(m_li);
             }
-
-
-
             //Add the options now for each question
             for (int j=0; j < optionsArray.length(); j++) {
                 JSONObject jo_option_inside = optionsArray.getJSONObject(j);
-
                 String option1 = jo_option_inside.getString("option1");
                 String option2 = jo_option_inside.getString("option2");
                 String option3 = jo_option_inside.getString("option3");
                 String option4 = jo_option_inside.getString("option4");
-
-
                 questions.get(j).addOption(option1);
                 questions.get(j).addOption(option2);
                 questions.get(j).addOption(option3);
                 questions.get(j).addOption(option4);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        System.out.println("ADEL");
 
+    }
+    public void populateData2() {
+        try {
+            JSONObject obj = new JSONObject(loadJSON2FromAsset());
+            JSONArray questionArray = obj.getJSONArray("questions");
+            JSONArray optionsArray = obj.getJSONArray("choices");
+
+            for (int i = 0; i < questionArray.length(); i++) {
+                JSONObject jo_inside = questionArray.getJSONObject(i);
+                //Log.d("Details-->", jo_inside.getString("formule"));
+                int question_id = jo_inside.getInt("id");
+                String title = jo_inside.getString("title");
+                int correctAnswerId = jo_inside.getInt("correctAnswerId");
+
+                Question newQuestion = new Question(question_id, title, correctAnswerId);
+                questions.add(newQuestion);
+            }
+            //Add the options now for each question
+            for (int j=0; j < optionsArray.length(); j++) {
+                JSONObject jo_option_inside = optionsArray.getJSONObject(j);
+                String option1 = jo_option_inside.getString("option1");
+                String option2 = jo_option_inside.getString("option2");
+                String option3 = jo_option_inside.getString("option3");
+                String option4 = jo_option_inside.getString("option4");
+                questions.get(j).addOption(option1);
+                questions.get(j).addOption(option2);
+                questions.get(j).addOption(option3);
+                questions.get(j).addOption(option4);
             }
         } catch (JSONException e) {
             e.printStackTrace();
